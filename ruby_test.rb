a87a440f478b6596ea50b7b956b0b427a936ddd0
lahl82 <luisadolfo.hernandezlara@gmail.com>
2022-01-14 17:16:38 -0400

    Ruby and React Tests refactoring

--- ruby_test.rb
+++ ruby_test.rb
@@ -2,6 +2,17 @@
 #Hint: Remember, Do it in one sentence.
 
 ["a", nil, "b", "c", nil].reject(&:nil?).zip((["d", nil, "e", "f"].reject(&:nil?).reverse)).map{|e| e.join}
+#Opcion 1
+
+
+["a", nil, "b", "c", nil].compact.zip(["d", nil, "e", "f"].compact.reverse).map{|e| e.join}
+#Opcion 2
+
+a = ["a", nil, "b", "c", nil]
+b = ["d", nil, "e", "f"]
+
+a.compact.zip(b.compact.reverse).map{|e| e.join}
+#Opcion 3
 
 
 
